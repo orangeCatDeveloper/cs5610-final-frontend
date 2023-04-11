51014a11618c85663d096b0dcc25ecc963356581
calmix <rxsz123@163.com>
2023-04-11 16:15:21 -0400

    change review file name

---
+++ src/constants/path.js
@@ -0,0 +1,11 @@
+//
+export const ROUTE_PATHS = {
+    SEARCH: "/search",
+    USER_DETAIL: "/user-detail",
+    LOGIN: "/login",
+    HOME: "/home",
+    PROFILE: "/profile",
+    EDIT_PROFILE: "/edit-profile",
+    ACTIVITIES: "/activites",
+    SIGNUP: "/signup",
+};
---
+++ src/news/activity/review.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Review = () => {
+    return(
+        <div>
+            <h4>Review</h4>
+        </div>
+        
+    )
+}
+
+export default Review;
