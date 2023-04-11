6666c3acc9d79333fc9fbe6f5d24125f98355bea
orangeCatDeveloper <mrlemon00@163.com>
2023-04-11 14:38:25 -0400

    add page detail initial code

--- src/news/news-detail/index.js
+++ src/news/news-detail/index.js
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 const NewsDetail = () => {
+    const { id } = useParams();
+    const [newsData, setNewsData] = useState([]);
+
+    useEffect(() => {
+        // fetch news data
+        // fetch bookmark data
+        // fetch review data
+    }, []);
+
+
+
     return (
         <div>
             <h4>Detail</h4>
